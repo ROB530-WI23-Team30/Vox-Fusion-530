e718f8c7304ab494f102c0ebb3b1af61b9d01d21
cc <cctom@umich.edu>
2023-04-05 21:29:28 -0400

    fix, feat: ethl config file and data loader fix

--- src/dataset/ethl.py
+++ src/dataset/ethl.py
@@ -17,18 +17,25 @@ class DataLoader(Dataset):
         self.K = self.load_intrinsic()
         self.gt_pose = self.load_gt_pose()
 
+    def get_init_pose(self):
+        if self.gt_pose is not None:
+            return self.gt_pose[0, ...].reshape(4, 4)
+        else:
+            return np.eye(4)
+
     def load_intrinsic(self):
         return np.array([[481.2, 0, 319.5], [0, 480.0, 239.5], [0, 0, 1]])
 
     def load_gt_pose(self):
         gt_file = osp.join(self.data_path, "groundtruth.txt")
         pose_data = np.loadtxt(gt_file, delimiter=" ", dtype=np.unicode_)
-        pose_vecs = pose_data[:, 1:].astype(np.float64)
+        pose_vecs = pose_data[:, 1:]
         poses = [
-            torch.from_numpy(DataLoader.pose_matrix_from_quaternion(p))
-            for p in pose_vecs
+            DataLoader.pose_matrix_from_quaternion(p).reshape(-1) for p in pose_vecs
         ]
         assert len(poses) == self.num_imgs, "number of poses and images mismatch"
+        poses = np.array(poses, dtype=np.float64)
+        return poses
 
     @staticmethod
     def pose_matrix_from_quaternion(pvec: np.ndarray) -> np.ndarray:
@@ -45,22 +52,21 @@ class DataLoader(Dataset):
 
     def load_image(self, index):
         rgb = cv2.imread(osp.join(self.data_path, "rgb", "{:04d}.png".format(index)))
-        rgb = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
-        depth = cv2.imread(
-            osp.join(self.data_path, "depth", "{:04d}.png".format(index))
+        depth: np.ndarray = cv2.imread(
+            osp.join(self.data_path, "depth", "{:04d}.png".format(index)), -1
         )
         depth = depth / 5000.0
         # if self.max_depth > 0:
         #     depth[depth > self.max_depth] = 0
-        return (rgb / 255.0).float, depth.float()
+        return (rgb / 255.0), depth
 
     def __len__(self):
         return self.num_imgs
 
     def __getitem__(self, index):
         img, depth = self.load_image(index)
-        img = torch.from_numpy(img)
-        depth = torch.from_numpy(depth)
+        img = torch.from_numpy(img).float()
+        depth = torch.from_numpy(depth).float()
         pose = self.gt_pose[index] if self.use_gt else None
         return index, img, depth, self.K, pose
 
@@ -69,10 +75,14 @@ if __name__ == "__main__":
     import sys
 
     loader = DataLoader(sys.argv[1])
-    for data in loader:
-        index, img, depth, K, _ = data
+    num_img = len(loader)
+    print(f"loader length: {len(loader)}")
+    for i in range(num_img):
+        index, img, depth, K, _ = loader[i]
         print(K)
         print(index, img.shape)
+        print(index, depth.shape)
         cv2.imshow("img", img.numpy())
         cv2.imshow("depth", depth.numpy())
+        print(f"depth: {depth.reshape(-1)[:10]}")
         cv2.waitKey(1)
